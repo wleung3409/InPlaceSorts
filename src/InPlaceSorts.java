836b2a79244cd4835f43e2280e54bdb19315cab5
William <wleung3409@bths.edu>
2019-01-14 22:41:29 -0500

    done

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -46,7 +46,16 @@ public class InPlaceSorts {
     }
 
     public static void insertionSort(int[] list1) {
-
+        for (int i = 0; i < list1.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (list1[i] < list1[j]) {
+                    swap(list1, i, j);
+                }
+                else {
+                    j = 0;
+                }
+            }
+        }
     }
 
     public static int[] randIntArr(int count) {
@@ -60,7 +69,7 @@ public class InPlaceSorts {
     public static double[] randDoubleArr(int count) {
         double[] randomArr = new double[count];
         for (int i = 0; i < randomArr.length; i++) {
-            randomArr[i] = (int) (Math.random() * 10001);
+            randomArr[i] = Math.random() * 10001;
         }
         return randomArr;
     }
@@ -81,9 +90,51 @@ public class InPlaceSorts {
         return randomArr;
     }
 
+    public static int[] copyIntArray(int[] list1) {
+        int[] list2 = new int[list1.length];
+        for (int i = 0; i < list1.length; i++) {
+            list2[i] = list1[i];
+        }
+        return list2;
+    }
+
+    public static double[] copyDoubleArray(double[] list1) {
+        double[] list2 = new double[list1.length];
+        for (int i = 0; i < list1.length; i++) {
+            list2[i] = list1[i];
+        }
+        return list2;
+    }
+
+    public static String[] copyStringArray(String[] list1) {
+        String[] list2 = new String[list1.length];
+        for (int i = 0; i < list1.length; i++) {
+            list2[i] = list1[i];
+        }
+        return list2;
+    }
+
     public static boolean isSorted(int[] list1) {
         for (int i = 0; i < list1.length - 1; i++) {
             if (list1[i] > list1[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSorted(double[] list1) {
+        for (int i = 0; i < list1.length - 1; i++) {
+            if (list1[i] > list1[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSorted(String[] list1) {
+        for (int i = 0; i < list1.length - 1; i++) {
+            if (list1[i].compareTo(list1[i+1]) > 0) {
                 return false;
             }
         }
@@ -105,6 +156,49 @@ public class InPlaceSorts {
             }
             return true;
         }
+    }
+
+    public static boolean checkSum(double[] before, double[] after) {
+        double sum1 = 0;
+        double sum2 = 0;
+        if (before.length != after.length) {
+            return false;
+        } else {
+            for (int i = 0; i < before.length; i++) {
+                sum1 += before[i];
+                sum2 += after[i];
+            }
+            if (sum1 != sum2) {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public static boolean checkSum(String[] before, String[] after) {
+        if (before.length != after.length) {
+            return false;
+        }
+        int sum1 = 0;
+        int x = 0;
+        int sum2 = 0;
+        int y = 0;
+        for (int i = 0; i < before.length; i++ ) {
+            for (int j = 0; j < before[i].length(); j++) {
+                x += (int) before[i].charAt(j);
+                sum1 += x;
+            }
+        }
+        for (int n = 0; n < after.length; n++) {
+            for (int m = 0; m < after[n].length(); m++) {
+                y += (int) after[n].charAt(m);
+                sum2 += y;
+            }
+        }
+        if (sum1 != sum2) {
+            return false;
+        }
+        return true;
     }
 
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,34 +1,105 @@
 public class Runner {
     public static void main(String[] args) {
-        int[] testArr = SortingUtil.randIntArr(10);
-        int[] finalArr = testArr;
-        System.out.print("Before: ");
-        System.out.println();
-        for (int num:testArr) {
-            System.out.print(num + " ");
+        int[] intArr = InPlaceSorts.randIntArr(5);
+        int[] copyArr1 = InPlaceSorts.copyIntArray(intArr);
+        double[] doubleArr = InPlaceSorts.randDoubleArr(5);
+        double[] copyArr2 = InPlaceSorts.copyDoubleArray(doubleArr);
+        String[] stringArr = InPlaceSorts.randStringArr(5,5);
+        String[] copyArr3 = InPlaceSorts.copyStringArray(stringArr);
+
+
+        System.out.println("Bubble Sort");
+        System.out.println("Before: ");
+        for (String str:stringArr) {
+            System.out.print(str + " ");
             System.out.println();
         }
         long time = System.nanoTime();
-        SortingUtil.bubbleSort(finalArr);
+        InPlaceSorts.bubbleSort(copyArr3);
         time = System.nanoTime() - time;
+        System.out.println();
         System.out.print("After: ");
         System.out.println();
-        for (int num:testArr) {
-            System.out.print(num + " ");
+        for (String str:copyArr3) {
+            System.out.print(str + " ");
             System.out.println();
         }
-        if (!SortingUtil.isSorted(testArr) && !SortingUtil.checkSum(finalArr, testArr)) {
+        if (!InPlaceSorts.isSorted(copyArr3) && !InPlaceSorts.checkSum(stringArr, copyArr3)) {
+            System.out.println("Something went wrong in the sorting method that the strings are not correctly sorted and there are different strings within your previous array and the array after the sorting method!");
+        }
+        else if (!InPlaceSorts.isSorted(copyArr3) && InPlaceSorts.checkSum(stringArr, copyArr3)) {
+            System.out.println("All of your strings within your array are the same as before but something went wrong in your sorting method that they are not correctly sorted!");
+        }
+        else if (InPlaceSorts.isSorted(copyArr3) && !InPlaceSorts.checkSum(stringArr, copyArr3)) {
+            System.out.println("All of your strings are correctly sorted but something went wrong in your sorting method that there are different strings within your previous array and the array after the sorting method!");
+        }
+        else {
+            System.out.println("All of your strings are correctly sorted and contain all of the same strings within the previous array and the array after the sorting method!");
+        }
+        System.out.println("Time taken: " + time + " nanoseconds");
+        System.out.println();
+
+        System.out.println("Selection Sort");
+        System.out.println("Before: ");
+        for (double doub:doubleArr) {
+            System.out.print(doub + " ");
+            System.out.println();
+        }
+        long value = System.nanoTime();
+        InPlaceSorts.selectionSort(copyArr2);
+        value = System.nanoTime() - value;
+        System.out.println();
+        System.out.print("After: ");
+        System.out.println();
+        for (double doub:copyArr2) {
+            System.out.print(doub + " ");
+            System.out.println();
+        }
+        if (!InPlaceSorts.isSorted(copyArr2) && !InPlaceSorts.checkSum(doubleArr, copyArr2)) {
             System.out.println("Something went wrong in the sorting method that the numbers are not correctly sorted and there are different numbers within your previous array and the array after the sorting method!");
         }
-        else if (!SortingUtil.isSorted(testArr) && SortingUtil.checkSum(finalArr, testArr)) {
+        else if (!InPlaceSorts.isSorted(copyArr2) && InPlaceSorts.checkSum(doubleArr, copyArr2)) {
             System.out.println("All of your numbers within your array are the same as before but something went wrong in your sorting method that they are not correctly sorted!");
         }
-        else if (SortingUtil.isSorted(testArr) && !SortingUtil.checkSum(finalArr, testArr)) {
+        else if (InPlaceSorts.isSorted(copyArr2) && !InPlaceSorts.checkSum(doubleArr, copyArr2)) {
             System.out.println("All of your numbers are correctly sorted but something went wrong in your sorting method that there are different numbers within your previous array and the array after the sorting method!");
         }
         else {
             System.out.println("All of your numbers are correctly sorted and contain all of the same numbers within the previous array and the array after the sorting method!");
         }
-        System.out.println("Time taken: " + time + " nanoseconds");
+        System.out.println("Time taken: " + value + " nanoseconds");
+        System.out.println();
+
+        System.out.println("Insertion Sort");
+        System.out.println("Before: ");
+        for (int num:intArr) {
+            System.out.print(num + " ");
+            System.out.println();
+        }
+        long amount = System.nanoTime();
+        InPlaceSorts.insertionSort(copyArr1);
+        amount = System.nanoTime() - amount;
+        System.out.println();
+        System.out.print("After: ");
+        System.out.println();
+        for (int num:copyArr1) {
+            System.out.print(num + " ");
+            System.out.println();
+        }
+        if (!InPlaceSorts.isSorted(copyArr1) && !InPlaceSorts.checkSum(intArr, copyArr1)) {
+            System.out.println("Something went wrong in the sorting method that the numbers are not correctly sorted and there are different numbers within your previous array and the array after the sorting method!");
+        }
+        else if (!InPlaceSorts.isSorted(copyArr1) && InPlaceSorts.checkSum(intArr, copyArr1)) {
+            System.out.println("All of your numbers within your array are the same as before but something went wrong in your sorting method that they are not correctly sorted!");
+        }
+        else if (InPlaceSorts.isSorted(copyArr1) && !InPlaceSorts.checkSum(intArr, copyArr1)) {
+            System.out.println("All of your numbers are correctly sorted but something went wrong in your sorting method that there are different numbers within your previous array and the array after the sorting method!");
+        }
+        else {
+            System.out.println("All of your numbers are correctly sorted and contain all of the same numbers within the previous array and the array after the sorting method!");
+        }
+        System.out.println("Time taken: " + amount + " nanoseconds");
+
     }
+
 }
